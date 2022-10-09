14426eb3d9783c66dafcc5a6f404e0833ab23c19
aborn <loveaborn@foxmail.com>
2022-10-09 14:11:09 +0800

    playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,46 @@
 import { defineNuxtConfig } from 'nuxt/config'
 import nantaui from '..'
 
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import IconsResolver from 'unplugin-icons/resolver';
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineNuxtConfig({
   modules: [
-    nantaui
+    nantaui,
+    'nuxt-icon'
   ],
-  nantaui: {
-    addPlugin: true
-  }
+    vite: {
+        ssr: {
+            noExternal: ['ant-design-vue', 'dayjs'],
+        },
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                    // https://www.antdv.com/docs/vue/customize-theme/#Ant-Design-Vue-Less-variables
+                    // modifyVars: antdTheme(),
+                },
+            },
+        },
+        plugins: [
+            Components({
+                resolvers: [
+                    IconsResolver({
+                        prefix: 'Icon',
+                    }),
+                    // resolveIcons true will error with NITRO_PRESET=cloudflare
+                    AntDesignVueResolver({ resolveIcons: false, importStyle: 'less' }),
+                ],
+                dts: 'types/components.d.ts',
+            }),
+        ],
+        esbuild: isDev
+            ? {}
+            : {
+                pure: !isDev ? ['console.log', 'console.warn', 'debugger'] : [],
+                legalComments: 'none',
+            },
+    },
 })
---
+++ playground/pages/nanta/form/basic.vue
@@ -0,0 +1,57 @@
+<template>
+    <NantaForm
+      v-bind="getFormProps"
+      :actionColOptions="{ span: 24 }"
+      @register="registerForm"
+      @submit="handleSubmit"
+      @reset="handleReset"
+    />
+  </template>
+  
+  <script lang="ts" setup>
+  import { useForm, FormProps, Recordable } from "nanta-ui";
+  import { schemes } from "../table/data";
+  const [
+    registerForm,
+    { setFieldsValue, updateSchema, resetFields, getFieldsValue, validateFields },
+  ] = useForm({
+    labelWidth: 120,
+    schemas: schemes,
+    actionColOptions: {
+      span: 24,
+    },
+  });
+  
+  const customizeResetFn = (): Promise<void> => {
+    console.log("here is a customize resetFn called!");
+    setFieldsValue({
+      age: 0,
+      email: "nanta@sancaiwulian.com",
+      name: "Nanta",
+    });
+    return new Promise((resolve, reject) => {
+      try {
+        resolve();
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
+  
+  const formProps: FormProps = {
+    resetFn: customizeResetFn,
+    colon: true,
+  };
+  
+  const getFormProps = computed((): Recordable => ({ ...formProps }));
+  
+  const handleSubmit = (values: any) => {
+    console.log("values", values);
+    console.log("handleSubmit btn clicked!");
+  };
+  
+  const handleReset = () => {
+    console.log("handleReset but click");
+  };
+  </script>
+  
